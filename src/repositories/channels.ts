0ea00037e46df11e70b4f71c1575868dc9081d5b
Jeferson Costa <jefersonbrz.costa@gmail.com>
2021-05-23 17:55:36 -0300

    error handler

--- src/repositories/channels.ts
+++ src/repositories/channels.ts
@@ -19,26 +19,30 @@ const getChannel = async (channel: string) => {
   try {
     const page = await got(url)
     const content = new JSDOM(page.body)
-    const channelName = titleFormat(content.window.document.title)
-    const time = [...content.window.document.querySelectorAll('.lileft')]
-    const allItems = [...content.window.document.querySelectorAll('.mw > li')]
-    const category = [
-      ...content.window.document.querySelectorAll('.licontent > h3'),
-    ]
-    allItems.forEach((element) => {
-      if (element.classList.value === 'divider') return
-      if (element.classList.value === 'subheader devicepadding') {
-        currentDay = element.innerHTML
-        return
-      }
-      list.push({
-        date: currentDay,
-        time: time[itemCount].innerHTML,
-        name: element.children[0].title,
-        category: category[itemCount].innerHTML,
+    const channelName =
+      titleFormat(content.window.document.title) || 'not found'
+    if (channelName !== 'not found') {
+      const time = [...content.window.document.querySelectorAll('.lileft')]
+      const allItems = [...content.window.document.querySelectorAll('.mw > li')]
+      const category = [
+        ...content.window.document.querySelectorAll('.licontent > h3'),
+      ]
+      allItems.forEach((element) => {
+        if (element.classList.value === 'divider') return
+        if (element.classList.value === 'subheader devicepadding') {
+          currentDay = element.innerHTML
+          return
+        }
+        list.push({
+          date: currentDay,
+          time: time[itemCount].innerHTML,
+          name: element.children[0].title,
+          category: category[itemCount].innerHTML,
+        })
+        itemCount++
       })
-      itemCount++
-    })
+    }
+
     return { [channelName]: list }
   } catch (error) {
     console.error(error)
